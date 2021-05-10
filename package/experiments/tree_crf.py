5734bb97a068ac15cb5853d7d5425c1f9c06b7c1
Vijay Viswanathan <vcomeinpeace@gmail.com>
2021-05-09 20:52:13 -0400

    Fix bugs in CRF code

--- package/experiments/tree_crf.py
+++ package/experiments/tree_crf.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.nn as nn
 from .mlp import MLP, make_multi_layer_perceptron
@@ -16,8 +17,9 @@ def unary_potential_product(unary_potentials):
 
     
 class TreeNLLLoss(nn.Module):
-    def __init__(self):
+    def __init__(self,num_classes):
         super(TreeNLLLoss,self).__init__()
+        self.num_classes=num_classes
 
     # def forward(self, log_likelihood):
     #     # ...implement the forward function for negative log likelihood loss...
@@ -30,7 +32,7 @@ class TreeNLLLoss(nn.Module):
             true_parent = node.true_label
             for child in node.children:
                 true_child = child.true_label
-                edge_pot = child.parent_edge_potential[0,len(tag_dict)*true_child+true_parent]
+                edge_pot = child.parent_edge_potential[0,self.num_classes*true_child+true_parent]
                 losses.append(torch.clone(edge_pot))
                 losses.append(torch.clone(child.unary_potential[0, true_child]))
                 loss = loss + edge_pot
@@ -45,7 +47,7 @@ class TreeNLLLoss(nn.Module):
         if len(losses) == 1:
             losses = losses[0]
         else:
-            losses = torch.cat(losses, dim=0)
+            losses = torch.stack(losses)
         return -1*(loss-partition_func)
 
 
@@ -57,32 +59,32 @@ class TreeCRF(nn.Module):
         self.build_unary_potentials = nn.Linear(hidden_dim, num_classes)
         self.build_edge_potentials = nn.Linear(2 * hidden_dim, num_classes * num_classes)
 
-        self.criterion = TreeNLLLoss()
         self.num_classes = num_classes
+        self.criterion = TreeNLLLoss(num_classes = num_classes)
         self.hidden_dim = hidden_dim
         self.input_dim = input_dim
         self.mlp = make_multi_layer_perceptron(in_dim=input_dim, out_dim=None, hidden_dim=hidden_dim, num_layers=num_layers, feature_extractor=True)
 
-    def build_traversal_and_get_features(self, node, traversal_list = [], features = []):
+    def build_traversal_and_get_features(self, node, traversal_list = [], features = {}):
         if len(node.children) > 0 and node not in traversal_list:
             for child in node.children:
-                if len(child.children) > 0 and child not in traversal_list:
+                if child not in traversal_list:
                     traversal_list, features = self.build_traversal_and_get_features(child, traversal_list, features)
+            print(f"node idx: {node.idx}")
             if node not in traversal_list:
                 traversal_list.append(node)
-                features.append(node.features)
+                features[node.idx] = node.features
+            return traversal_list, features
+        elif len(node.children) == 0 and node not in traversal_list:
+            print(f"node idx: {node.idx}")
+            features[node.idx] = node.features
             return traversal_list, features
 
     def predict(self, norm_beliefs, labels, node_idxs):
         full = torch.cat(norm_beliefs, dim=0)
-
         preds = torch.argmax(full, axis = 1)
-        mask = np.array([(p==a) for p,a in zip(preds, labels)]).astype(int)
-        tree_acc = mask.sum()/len(mask)
-        leaf_mask = mask[leaf_idxs]
-        leaf_acc = leaf_mask.sum()/len(leaf_idxs)
         preds_dict = dict(zip(node_idxs, preds))
-        return tree_acc, leaf_acc, preds_dict
+        return preds_dict
 
     def forward(self, tree):
         if len(tree.children) == 0:
@@ -93,22 +95,40 @@ class TreeCRF(nn.Module):
             tree.unary_potential = unary_pot
             return [tree]
 
-        traversal_list, features = self.build_traversal_and_get_features(tree, [], [])
-        features_tensor = torch.stack(features)
+        traversal_list, features = self.build_traversal_and_get_features(tree, [], {})
+        node_idxs = sorted(list(features.keys()))
+        node2ix = {n_idx: i for i, n_idx in zip(range(len(node_idxs)), node_idxs)}
+        print(node2ix)
+        ix2node = {i:n_idx for n_idx,i in node2ix.items()}
+
+        # {999: something, 1000: None}
+        # 0 , 1
+        features_list = []
+        sorted_indices = sorted(ix2node.keys())
+        for index in sorted_indices:
+            node_idx = ix2node[index]
+            features_list.append(features[node_idx])
+
+        features_tensor = torch.stack(features_list)
         try:
             hiddens = self.mlp(features_tensor)
         except:
             breakpoint()
         
-        for node, hidden in zip(traversal_list, hiddens):
+        for node in traversal_list:
             children = node.children
-            
+            hidden = hiddens[node2ix[node.idx]]
             node.hidden = torch.clone(hidden)
-
             unary_pot = torch.clone(self.build_unary_potentials(node.hidden))
-            node.unary_potential = unary_pot
+            node.unary_potential = unary_pot.view(1, -1)
             
             for child in children:
+                if len(child.children) == 0:
+                    # leaf node
+                    child_ix = node2ix[child.idx]
+                    child.hidden = torch.clone(hiddens[child_ix])
+                    unary_pot = torch.clone(self.build_unary_potentials(child.hidden))
+                    child.unary_potential = unary_pot.view(1, -1)
                 assert child.hidden is not None, breakpoint()
                 # parent_features = node.features[:self.hidden]
                 # parent_neighbors = node.features[self.hidden:]
@@ -117,8 +137,8 @@ class TreeCRF(nn.Module):
                 # shared_neighbor_features = parent_neighbors + child_neighbors - parent_features - child_features
 
                 #edge_pot = torch.clone(self.build_edge_potentials(torch.cat([parent_features, child_features, shared_neighbor_features], dim=1)))
-                edge_pot = torch.clone(self.build_edge_potentials(torch.cat([node.hidden, child.hidden], dim=1)))
-                child.parent_edge_potential = edge_pot
+                edge_pot = torch.clone(self.build_edge_potentials(torch.cat([hidden, child.hidden], dim=0)))
+                child.parent_edge_potential = edge_pot.view(1, self.num_classes*self.num_classes)
         return traversal_list 
 
 ####################################################################
@@ -134,7 +154,7 @@ class TreeCRF(nn.Module):
         sanity_check = None
         loss = 0
         for node in traversal_list:
-            # do forward pass
+            # do upward pass
             for child in node.children:
                 child_pot = torch.clone(child.unary_potential)
                 inc_msgs = []
@@ -152,12 +172,12 @@ class TreeCRF(nn.Module):
                 child.message_to_parent = torch.clone(msg_to_parent).view(1, msg_to_parent.shape[0])
         
         for node in traversal_list[-1::-1]:
-            # do backward pass (and compute beliefs??)
+            # do downward pass (and compute beliefs??)
             belief_tmp = [torch.clone(node.unary_potential)]
             if node.parent is not None:
                 assert node.message_from_parent is not None
                 belief_tmp.append(torch.clone(node.message_from_parent))
-            
+            child_node_idxs = set([i for i in range(len(node.children))])
             for i in range(len(node.children)): 
                 child = node.children[i]
                 assert child.message_to_parent is not None
@@ -168,13 +188,16 @@ class TreeCRF(nn.Module):
                     assert node.message_from_parent is not None
                     msg_from_p.append(torch.clone(node.message_from_parent))
                 if len(node.children) > 1:
-                    
-                    child2 = node.children[i+1-2*i]
-                    assert child2.message_to_parent is not None
-                    msg_from_p.append(torch.clone(child2.message_to_parent))
+                    for j in child_node_idxs-set([i]):
+                        child2 = node.children[j]
+                        assert child2.message_to_parent is not None
+                        msg_from_p.append(torch.clone(child2.message_to_parent))
                 # messages at parent 
                 tmp_ = torch.sum(torch.cat(msg_from_p, dim = 0), dim=0)
-                tmp_ = tmp_.view(1, tmp_.shape[0])
+                try:
+                    tmp_ = tmp_.view(1, tmp_.shape[0])
+                except:
+                    breakpoint()
                 assert child.message_at is not None
                 msg_at_child = torch.repeat_interleave(torch.clone(child.message_at), repeats=tmp_.shape[1], dim=1)
                 edge_tmp = child.parent_edge_potential
@@ -182,22 +205,28 @@ class TreeCRF(nn.Module):
                 #edge_belief = torch.sum(torch.cat([torch.cat(tmp_.shape[1]*[tmp_],dim=1), msg_at_child, edge_tmp], dim=0), dim=0)
                 
                 #print(torch.logsumexp(edge_belief, dim=0))
-                edge_pot = torch.clone(edge_tmp.view(len(tag_dict),len(tag_dict)))
+                edge_pot = torch.clone(edge_tmp.view(self.num_classes,self.num_classes))
                 tmp_ =torch.cat(edge_pot.shape[0]*[tmp_])
                 msg_from_parent = torch.logsumexp(edge_pot+tmp_, 1)
                 child.message_from_parent = torch.clone(msg_from_parent).view(1, msg_from_parent.shape[0])
                 if len(child.children) == 0:
                     child_belief_tmp = torch.sum(torch.cat([child.unary_potential, child.message_from_parent], dim=0), dim=0)
-                    assert torch.isclose(torch.logsumexp(child_belief_tmp, 0), sanity_check)
+                    partition_func = torch.logsumexp(child_belief_tmp, 0)
+
+                    if sanity_check is not None:
+                        assert torch.isclose(partition_func, sanity_check, rtol=1e-3, atol=1e-08), breakpoint()
+                    else:
+                        sanity_check = torch.clone(partition_func)
+
+                    assert torch.isclose(torch.logsumexp(child_belief_tmp, 0), sanity_check, rtol=1e-3, atol=1e-08), breakpoint()
                     child.belief = torch.clone(child_belief_tmp)
                     norm_beliefs.append((child_belief_tmp-sanity_check).view(1, child_belief_tmp.shape[0]))
                     labels.append(child.true_label)
                     
             belief_tmp = torch.sum(torch.cat(belief_tmp, dim = 0), dim=0)
             partition_func = torch.logsumexp(belief_tmp, dim=0)
-            #assert torch.isclose(torch.logsumexp(edge_belief, 0), partition_func)
             if sanity_check is not None:
-                assert torch.isclose(partition_func, sanity_check)
+                assert torch.isclose(partition_func, sanity_check, rtol=1e-3, atol=1e-08), breakpoint()
             else:
                 sanity_check = torch.clone(partition_func)
             node.belief = torch.clone(belief_tmp)
